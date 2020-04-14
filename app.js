263aca40e15404d92896c2e56426153e516e9549
JoRan29 <jokavana29@gmail.com>
2020-04-14 21:04:14 +0200

    bubble sort

--- app.js
+++ app.js
@@ -65,4 +65,32 @@ const insertionSort = (arr) => {
   return arr;
 };
 
-console.log(insertionSort(arr));
+// console.log(insertionSort(arr));
+
+let arr2 = [3, 1, 7];
+
+console.log(insertionSort(arr2));
+
+// Bubble Sort
+
+function bubbleSort(arr) {
+  //loop length
+  const loop = arr.length;
+  //loop for loop length
+  for (let i = 0; i < loop; i++) {
+    //cycle through arr items
+    for (let j = 0; j < loop; j++) {
+      //compare adjacent items
+      if (arr[j] > arr[j + 1]) {
+        //swap
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  console.log(arr);
+  return arr;
+}
+
+bubbleSort([45, 101, -10, 55522, 3224, 523, 16, 133, 543, 33, 33, 123]);
